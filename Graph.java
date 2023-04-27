42995029cf98ad1add3a3d45116042faddd1956d
Arush Gupta <arushgupta.dev@gmail.com>
2023-04-27 04:24:24 -0500

    Implemented SLVO, randomOrder, LLDO, SODO, LODO, graph coloring

--- Graph.java
+++ Graph.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Graph {
@@ -9,10 +11,13 @@ public class Graph {
         int color;
         List<Integer> neighbors;
 
+        Vertex prev;
+        Vertex next;
+
         public Vertex(int id, int size) {
             this.id = id;
             this.degree = 0;
-            this.color = -1;
+            this.color = 0;
             this.neighbors = new ArrayList<Integer>(size);
         }
 
@@ -37,6 +42,7 @@ public class Graph {
         for (int i = 0; i < size; i++) {
             vertices.add(i, new Vertex(i, size));
         }
+        ordering = new ArrayList<Vertex>(size);
     }
 
     public void addEdge(int src, int dest) {
@@ -165,14 +171,142 @@ public class Graph {
         }
     }
 
-    public void performSLVO() {
-        List<Vertex> degrees = new ArrayList<Vertex>(size);
+    // Smallest Last Vertex Ordering
+    public void SLVO() {
+        Vertex[] degrees = new Vertex[size];
         int[] newDegrees = new int[size];
-        int[] deleted = new int[size];
+        boolean[] deleted = new boolean[size];
 
+        for (int i = 0; i < vertices.size(); i++) {
+            Vertex vertex = vertices.get(i);
+            if (degrees[vertex.degree] != null) {
+                degrees[vertex.degree].prev = vertex;
+                vertex.next = degrees[vertex.degree];
+            }
+            degrees[vertex.degree] = vertex;
+            newDegrees[i] = vertex.degree;
+        }
         
+        for(int i = 0; i < degrees.length; i++) {
+            if(degrees[i] == null) {
+                continue;
+            }
+            Vertex temp = degrees[i];
+            ordering.add(temp);
+            degrees[i] = temp.next;
 
+            if (temp.next != null) {
+                temp.next.prev = null;
+            }
+
+            temp.next = null;
+            deleted[temp.id] = true;
+
+            for(int neighborId: temp.neighbors) {
+                if(deleted[neighborId] == true) {
+                    continue;
+                }
+                Vertex neighbor = vertices.get(neighborId);
+                if(neighbor.next != null) {
+                    neighbor.next.prev = neighbor.prev;
+                }
+                if (neighbor.prev != null) {
+                    neighbor.prev.next = neighbor.next;
+                } else {
+                    degrees[newDegrees[neighborId]] = neighbor.next;
+                }
+                newDegrees[neighborId] -= 1;
+                neighbor.prev = null;
+                neighbor.next = degrees[newDegrees[neighborId]];
+
+                if (degrees[newDegrees[neighborId]] != null) {
+                    degrees[newDegrees[neighborId]].prev = neighbor;
+                }
+                degrees[newDegrees[neighborId]] = neighbor;
+            }
+            if (i == 0) {
+                i--;
+                continue;
+            }
+            i -= 2;
+        }
+        Collections.reverse(ordering);
     }
 
+    // Largest Last Degree Ordering
+    public void LLDO() {
+        SLVO();
+        Collections.reverse(ordering);
+    }
 
+    // Random Vertex Ordering
+    public void randomOrder() {
+        for(Vertex v: vertices) {
+            ordering.add(v);
+        }
+        Collections.shuffle(ordering);
+    }
+
+    // Smallest Original Degree Ordering
+    public void SODO() {
+        Vertex[] degrees = new Vertex[size];
+
+        for (int i = 0; i < vertices.size(); i++) {
+            Vertex vertex = vertices.get(i);
+            if (degrees[vertex.degree] != null) {
+                degrees[vertex.degree].prev = vertex;
+                vertex.next = degrees[vertex.degree];
+            }
+            degrees[vertex.degree] = vertex;
+        }
+
+        for(int i = 0; i < degrees.length; i++) {
+            Vertex curr = degrees[i];
+            while (curr != null) {
+                ordering.add(curr);
+                curr = curr.next;
+            }
+        }
+    }
+
+    // Largest Original Degree Ordering
+    public void LODO() {
+        SODO();
+        Collections.reverse(ordering);
+    }
+
+    public void colorGraph() {
+        for (Vertex vertex : ordering) {
+            vertex.color = colorVertex(vertex);
+            if (vertex.color > maxColor) {
+                maxColor = vertex.color;
+            }
+        }
+    }
+
+    public int colorVertex(Vertex vertex) {
+        int[] colors = new int[size];
+
+        for (int i = 0; i < vertex.neighbors.size(); i++) {
+            int neighbor = vertex.neighbors.get(i);
+            colors[i] = neighbor;
+        }
+        
+        int num = 0;
+        for (int i = 0; i < colors.length; i++) {
+            num = colors[i];
+            while(0 < num && num <= colors.length && colors[num - 1] != num) {
+                int temp = colors[i];
+                colors[i] = colors[num - 1];
+                colors[num - 1] = temp;
+                num = colors[i];
+            }
+        }
+        for (int i = 0; i < colors.length; i++) {
+            if (colors[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return colors.length + 1;
+    }
 }
--- Solution.java
+++ Solution.java
@@ -1,19 +1,34 @@
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
-        int size = 10000;
+        int size = 5;
         int E = 200;
         Graph graph = new Graph(size);
 
-        long startTime = System.currentTimeMillis();
+        // graph.addEdge(0,1);
+        // graph.addEdge(1, 2);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+
+        graph.SLVO();
+        graph.colorGraph();
+
+        // long startTime = System.currentTimeMillis();
         // graph.createCompleteGraph();
         // graph.createCycleGraph();
         // graph.createUniformDistGraph(E);
-        graph.createSkewedDistGraph(size * E);
+        // graph.createSkewedDistGraph(size * E);
         // graph.createMyDistGraph(E);
-        long endTime = System.currentTimeMillis();
+        // long endTime = System.currentTimeMillis();
 
-        // graph.printGraph();
-        System.out.println("Time Taken: " + (endTime - startTime));
+        graph.printGraph();
+        // System.out.println("Time Taken: " + (endTime - startTime));
 
     }
 }
